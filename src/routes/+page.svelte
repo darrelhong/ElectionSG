9b78d10c3644b0bd0d0d4fa7712efab0ec91a5a6
darrelhong <darrel.hong@gmail.com>
2024-08-18 08:50:09 +0800

    add map styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,17 @@
 	import maplibregl from 'maplibre-gl';
 	import 'maplibre-gl/dist/maplibre-gl.css';
 	import { onMount } from 'svelte';
+	import { positronMapStyle, datavizMapStyle } from '$lib/map-style';
 
 	onMount(() => {
 		const map = new maplibregl.Map({
 			container: 'map',
-			style: 'https://demotiles.maplibre.org/style.json',
+			style: datavizMapStyle, // positronMapStyle
 			center: [103.809, 1.319],
 			zoom: 10
 		});
-        
-        map.fitBounds([103.600, 1.240, 104.034, 1.469]);
+
+		map.fitBounds([103.6, 1.24, 104.034, 1.469]);
 	});
 </script>
 
