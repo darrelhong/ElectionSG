07fb03ecd3cfe6475ed4e52995f37287dc850b89
darrelhong <darrel.hong@gmail.com>
2024-08-20 22:08:08 +0800

    add button component

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import clsx from 'clsx';
+</script>
+
+<button
+	class={clsx(
+		'bg-black text-white hover:bg-black/85 ',
+		'text-sm font-medium',
+		'px-4 py-2',
+		'inline-flex items-center justify-center whitespace-nowrap rounded-md transition-colors',
+		'focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-black',
+		'disabled:pointer-events-none disabled:opacity-50',
+		$$props.class
+	)}><slot /></button
+>
