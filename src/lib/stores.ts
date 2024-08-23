ace061c4f01c1b0de26754991ccbb010afe27c44
darrelhong <darrel.hong@gmail.com>
2024-08-23 21:22:54 +0800

    add year selector

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,13 @@
-import { writable } from 'svelte/store';
+import { derived, writable } from 'svelte/store';
 
 export const selectedDivision = writable<string | null>(null);
+
+export const selectedYearOption = writable({
+	value: '2020',
+	label: '2020'
+});
+
+export const selectedYear = derived(
+	selectedYearOption,
+	($selectedYearOption) => $selectedYearOption.value
+);
--- src/routes/info.svelte
+++ src/routes/info.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
-	import { selectedDivision } from '$lib/stores';
+	import { selectedDivision, selectedYear } from '$lib/stores';
+	import YearSelect from './year-select.svelte';
 </script>
 
 <div class="pointer-events-none fixed inset-0 grid items-end p-4">
-	<div class="card pointer-events-auto">
+	<div class="card pointer-events-auto grid gap-y-2">
 		{#if $selectedDivision}
 			<p>Selected constituency: {$selectedDivision}</p>
 		{:else}
 			<p>Select a constituency to continue</p>
 		{/if}
+
+		<YearSelect />
 	</div>
 </div>
