d5d7d3ff20cadd3586448bc62144faf5dd8bf735
darrelhong <darrel.hong@gmail.com>
2024-08-28 23:44:59 +0800

    set max height for content and reduce padding

--- src/routes/info.svelte
+++ src/routes/info.svelte
@@ -7,7 +7,7 @@
 	$: divisionResults = $selectedDivision && getParsedResults($selectedYear)[$selectedDivision];
 </script>
 
-<div class="pointer-events-none fixed inset-0 grid items-end p-4">
+<div class="pointer-events-none fixed inset-0 grid items-end p-2">
 	<div class="card pointer-events-auto grid gap-y-2">
 		{#if divisionResults}
 			<div class="flex">
@@ -33,26 +33,28 @@
 				>
 			</div>
 
-			{#each divisionResults.parties as party}
-				<div class="rounded-md border p-4">
-					<p><span class="font-medium">Party:</span> {party.party}</p>
+			<div class="grid max-h-[60dvh] gap-y-2 overflow-y-auto">
+				{#each divisionResults.parties as party}
+					<div class="rounded-md border p-4">
+						<p><span class="font-medium">Party:</span> {party.party}</p>
 
-					<p class="inline-block">
-						<span class="font-medium">Candidates:</span>
-						<span>
-							{#each party.candidates as candidate, i}
-								{candidate}{i !== party.candidates.length - 1 ? ' | ' : ''}
-							{/each}
-						</span>
-					</p>
+						<p class="inline-block">
+							<span class="font-medium">Candidates:</span>
+							<span>
+								{#each party.candidates as candidate, i}
+									{candidate}{i !== party.candidates.length - 1 ? ' | ' : ''}
+								{/each}
+							</span>
+						</p>
 
-					<p>
-						<span class="font-medium">Votes:</span>
-						<span>{party.vote_count.toLocaleString('en-SG')}</span>
-						<span>({(party.vote_percentage * 100).toFixed(2)}%)</span>
-					</p>
-				</div>
-			{/each}
+						<p>
+							<span class="font-medium">Votes:</span>
+							<span>{party.vote_count.toLocaleString('en-SG')}</span>
+							<span>({(party.vote_percentage * 100).toFixed(2)}%)</span>
+						</p>
+					</div>
+				{/each}
+			</div>
 		{:else}
 			<p>Select a constituency to view results</p>
 		{/if}
