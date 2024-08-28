4d0ff11c5495ef49b62e5eadf55e919e85209fb1
darrelhong <darrel.hong@gmail.com>
2024-08-28 23:33:35 +0800

    add close button, button variant, and update results shown

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
 	import clsx from 'clsx';
+	import { cva, type VariantProps } from 'class-variance-authority';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	const button = cva(
+		clsx(
+			'text-sm font-medium',
+			'px-4 py-2',
+			'inline-flex items-center justify-center whitespace-nowrap rounded-md transition-colors',
+			'focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-black',
+			'disabled:pointer-events-none disabled:opacity-50'
+		),
+		{
+			variants: {
+				variant: {
+					default: clsx('bg-black text-white hover:bg-black/85'),
+					ghost: clsx('text-black hover:bg-black/5')
+				}
+			}
+		}
+	);
+	interface $$Props extends HTMLButtonAttributes, VariantProps<typeof button> {}
+
+	export let variant: $$Props['variant'] = 'default';
 </script>
 
-<button
-	class={clsx(
-		'bg-black text-white hover:bg-black/85 ',
-		'text-sm font-medium',
-		'px-4 py-2',
-		'inline-flex items-center justify-center whitespace-nowrap rounded-md transition-colors',
-		'focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-black',
-		'disabled:pointer-events-none disabled:opacity-50',
-		$$props.class
-	)}><slot /></button
->
+<button {...$$props} class={button({ variant, class: $$props.class })}><slot /></button>
--- src/routes/info.svelte
+++ src/routes/info.svelte
@@ -1,18 +1,37 @@
 <script lang="ts">
 	import Select from '$lib/components/select.svelte';
-	import { selectedDivision, selectedYearOption } from '$lib/stores';
-	import { parsedResults2020 } from '$lib/parsed';
+	import { selectedDivision, selectedYearOption, selectedYear } from '$lib/stores';
+	import { getParsedResults } from '$lib/parsed';
+	import Button from '$lib/components/button.svelte';
 
-	$: divisionResults = $selectedDivision && parsedResults2020[$selectedDivision];
+	$: divisionResults = $selectedDivision && getParsedResults($selectedYear)[$selectedDivision];
 </script>
 
 <div class="pointer-events-none fixed inset-0 grid items-end p-4">
 	<div class="card pointer-events-auto grid gap-y-2">
 		{#if divisionResults}
-			<h2 class="text-lg font-semibold">
-				{$selectedDivision}
-				{divisionResults.type}
-			</h2>
+			<div class="flex">
+				<h2 class="me-auto text-lg font-semibold">
+					{$selectedDivision}
+					{divisionResults.type}
+				</h2>
+
+				<Button variant="ghost" class="!p-2" onclick={() => selectedDivision.set(null)}>
+					<svg
+						width="15"
+						height="15"
+						viewBox="0 0 15 15"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+							fill="currentColor"
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+						></path></svg
+					></Button
+				>
+			</div>
 
 			{#each divisionResults.parties as party}
 				<div class="rounded-md border p-4">
